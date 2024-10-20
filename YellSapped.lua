7220d4f7caa22d5e6ece63c03241d6c80a657953
The-Sickness <alloycowboy6969@gmail.com>
2024-10-20 16:33:03 -0500

    Fixed Channel issues

--- YellSapped.lua
+++ YellSapped.lua
@@ -1,7 +1,3 @@
---YellSapped--
---Sharpedge_Gaming--
---v.1.6
-
 local AceAddon = LibStub("AceAddon-3.0"):NewAddon("YellSapped", "AceConsole-3.0", "AceEvent-3.0")
 local AceConfig = LibStub("AceConfig-3.0")
 local AceConfigDialog = LibStub("AceConfigDialog-3.0")
@@ -29,6 +25,7 @@ local messages = {
 local defaults = {
     profile = {
         selectedMessage = 1, -- Default to the first message
+        outputChannel = "YELL", -- Default to YELL channel
     },
 }
 
@@ -46,10 +43,27 @@ function AceAddon:OnInitialize()
             local _, event, _, _, sourceName, _, _, _, dstName, _, _, spellId = CombatLogGetCurrentEventInfo()
             if spellId == 6770 and dstName == UnitName("player") and (event == "SPELL_AURA_APPLIED" or event == "SPELL_AURA_REFRESH") then
                 local inInstance, instanceType = IsInInstance()
-                if (inInstance and (instanceType == "pvp" or instanceType == "arena")) or GetPVPInfo() then
-                    SendChatMessage(messages[addon.db.profile.selectedMessage], "YELL")
-                    DEFAULT_CHAT_FRAME:AddMessage("Sapped by: "..(sourceName or "(unknown)"))
+                local outputChannel = addon.db.profile.outputChannel
+                
+                -- Handle PvP-specific cases
+                if instanceType == "pvp" or instanceType == "arena" then
+                    if outputChannel == "PARTY" then
+                        outputChannel = "INSTANCE_CHAT" -- Use INSTANCE_CHAT for PvP instead of PARTY
+                    end
                 end
+                
+                -- Respect the user's selected output channel
+                if outputChannel == "PARTY" and not IsInGroup() then
+                    outputChannel = "SAY"  -- Default to SAY if not in a party
+                elseif outputChannel == "RAID" and not IsInRaid() then
+                    outputChannel = "SAY"  -- Default to SAY if not in a raid
+                elseif outputChannel == "GUILD" and not IsInGuild() then
+                    outputChannel = "SAY"  -- Default to SAY if not in a guild
+                end
+
+                -- Send the message to the selected channel or the fallback channel
+                SendChatMessage(messages[addon.db.profile.selectedMessage], outputChannel)
+                DEFAULT_CHAT_FRAME:AddMessage("Sapped by: "..(sourceName or "(unknown)"))
             end
         end
     end)
@@ -73,7 +87,6 @@ function AceAddon:GetOptions()
                 get = function(info) return self.db.profile.selectedMessage end,
                 set = function(info, newValue)
                     self.db.profile.selectedMessage = newValue
-                    
                 end,
             },
             messagePreview = {
@@ -82,29 +95,27 @@ function AceAddon:GetOptions()
                 name = function()
                     return "Preview: " .. messages[self.db.profile.selectedMessage]
                 end,
-				},
-				outputChannel = {
-    order = 3,
-    type = "select",
-    name = "Output Channel",
-    desc = "Choose where to send the sapped message",
-    values = {
-    ["YELL"] = "Yell",
-    ["SAY"] = "Say",
-    ["PARTY"] = "Party",
-    ["RAID"] = "Raid",
-    ["GUILD"] = "Guild",
-    ["OFFICER"] = "Officer",
-
-    },
-    get = function(info) return self.db.profile.outputChannel end,
-    set = function(info, value)
-        self.db.profile.outputChannel = value
-    end,
+            },
+            outputChannel = {
+                order = 3,
+                type = "select",
+                name = "Output Channel",
+                desc = "Choose where to send the sapped message",
+                values = {
+                    ["YELL"] = "Yell",
+                    ["SAY"] = "Say",
+                    ["PARTY"] = "Party",
+                    ["RAID"] = "Raid",
+                    ["GUILD"] = "Guild",
+                    ["OFFICER"] = "Officer",
+                    ["INSTANCE_CHAT"] = "Instance",  -- Add INSTANCE_CHAT as an option
+                },
+                get = function(info) return self.db.profile.outputChannel end,
+                set = function(info, value)
+                    self.db.profile.outputChannel = value
+                end,
             },
         },
     }
     return options
 end
-
-
